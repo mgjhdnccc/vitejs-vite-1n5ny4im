7ea8af8b4fa9b9c4c1c1f068b51dd38bd9dd8bd8
5alikorkmaz <korkmaz65ali_@hotmail.com>
2025-05-05 19:39:56 +0300

    cypress.config2

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
-/*import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
-      // node eventleri buraya yazabilirsin (şart değil)
+      // node eventleri buraya yazabilirsin
     },
-  },
-}) */
+    supportFile: 'cypress/support/e2e.js'  // 👈 bunu ekliyoruz
+  }
+});
