76dc66273bf4d00c79c62a44d1278072cceab5ac
5alikorkmaz <korkmaz65ali_@hotmail.com>
2025-05-06 12:46:44 +0300

    loginCYjs

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,7 +12,7 @@ describe('Login Formu Testleri', () => {
     });
   
     it('Hatalı email girildiğinde: uyarı gösterilmeli, buton pasif olmalı', () => {
-      cy.get('input[type="email"]').type('test.com'); // geçersiz email
+      cy.get('input[type="email"]').type('test.com'); // Geçersiz e-posta
       cy.get('input[type="password"]').type('Test1234');
       cy.get('input[type="checkbox"]').check();
   
@@ -23,30 +23,22 @@ describe('Login Formu Testleri', () => {
   
     it('Email ve şifre hatalıysa 2 hata mesajı görünmeli, buton disabled olmalı', () => {
       cy.get('input[type="email"]').type('yanlisemail');
-      cy.get('input[type="password"]').type('123'); // zayıf şifre
+      cy.get('input[type="password"]').type('123'); // Zayıf şifre
       cy.get('input[type="checkbox"]').check();
   
-      cy.get('button').click();
-  
-      cy.get('p').should('have.length', 2).then(($ps) => {
-        expect($ps.eq(0).text()).to.contain('Geçerli bir e-posta');
-        expect($ps.eq(1).text()).to.contain('Şifre min 8 karakter');
-      });
+      cy.get('p').should('have.length', 2);
+      cy.contains('Geçerli bir e-posta girmelisin.').should('exist');
+      cy.contains('Şifre min 8 karakter olmalı, harf ve rakam içermeli.').should('exist');
   
       cy.get('button').should('be.disabled');
-
-      it('Email ve şifre doğru ama checkbox seçili değilse buton disabled olmalı', () => {
-        // Geçerli email ve şifre giriyoruz
-        cy.get('input[type="email"]').type('test@example.com');
-        cy.get('input[type="password"]').type('Test1234');
-      
-        // Checkbox bilerek işaretlenmiyor!
-      
-        // Hata mesajı görünmemeli
-        cy.get('p').should('not.exist');
-      
-        // Buton yine de pasif olmalı çünkü checkbox seçilmedi
-        cy.get('button').should('be.disabled');
-      });      
     });
-  });  
+  
+    it('Email ve şifre doğru ama checkbox seçili değilse buton disabled olmalı', () => {
+      cy.get('input[type="email"]').type('test@example.com');
+      cy.get('input[type="password"]').type('Test1234');
+  
+      // Checkbox işaretlenmedi
+      cy.get('p').should('not.exist');
+      cy.get('button').should('be.disabled');
+    });
+  });    
